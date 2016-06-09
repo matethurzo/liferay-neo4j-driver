a1b6f984a2dc9fac10a061bad6130bbac73ebaa8
Máté Thurzó <mate.thurzo@liferay.com>
2016-06-09 17:05:56 +0200

    Remove unused import

--- src/main/java/com/liferay/neo4j/GraphDatabase.java
+++ src/main/java/com/liferay/neo4j/GraphDatabase.java
@@ -27,7 +27,6 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
 /**
